98ca0b490f297c9428e78af7fb8ac7ab9309d548
Evgeniy <e.pogrebnyak@gmail.com>
2020-11-08 13:53:56 +0300

    formatted wth black

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,41 +4,49 @@ import pandas as pd
 
 st.set_page_config(
     page_title=" Demo App",
-    page_icon="🧊", # EP: how did they find a symbol?
+    page_icon="🧊",  # EP: how did they find a symbol?
     layout="wide",
     initial_sidebar_state="expanded",
 )
 
+
 def md_link(text: str, url: str):
-   return f"[{text}]({url})"
+    return f"[{text}]({url})"
+
 
 def api_docs(anchor):
-   url = f"https://docs.streamlit.io/en/stable/api.html#{anchor}"
-   text = f"docs - {anchor}"
-   st.markdown("---")
-   st.markdown(md_link(text, url))   
+    url = f"https://docs.streamlit.io/en/stable/api.html#{anchor}"
+    text = f"docs - {anchor}"
+    st.markdown("---")
+    st.markdown(md_link(text, url))
+
 
 st.sidebar.title("This is sidebar")
 
 st.title("Let's try Streamlit")
-st.markdown('[Streamlit](https://www.streamlit.io/) is a great way to make a dashboard, '
-            'interactive or not.')
+st.markdown(
+    "[Streamlit](https://www.streamlit.io/) is a great way to make a dashboard, "
+    "interactive or not."
+)
 
 col_a, col_b = st.beta_columns(2)
 
 with col_a:
-    st.subheader('Great features')
-    st.markdown("""
+    st.subheader("Great features")
+    st.markdown(
+        """
     - you just write a python script that will render as webpage
     - there is a sleek HTML template, no extra formatting needed 
     - interactivity - you can get feedback from widgets and controls
     - native methods to draw charts, render latex, insert graphviz
     - dedicated hosting (in beta)
-""")
+"""
+    )
 
 with col_b:
-    st.subheader('You can see Streamlit as')
-    st.markdown("""
+    st.subheader("You can see Streamlit as")
+    st.markdown(
+        """
 - [Jupyter notebook](https://jupyter.org/) less code cells
 - beefed-up [Handout][handout] (or Julia [Pluto.jl][pluto])
 - [Flask][flask] without template setup
@@ -50,9 +58,10 @@ with col_b:
 [pluto]: https://github.com/fonsp/Pluto.jl
 [handout]: https://github.com/danijar/handout    
 [flask]: https://flask.palletsprojects.com/en/1.1.x/
-    """)
- 
-st.header('Minimal example')
+    """
+    )
+
+st.header("Minimal example")
 
 """
 1. Install: 
@@ -86,34 +95,35 @@ st.header("Small examples")
 st.subheader("Input and display a number, show code")
 
 with st.echo():
-   x = st.number_input("A number please:")
-   st.write("Just got", x)
+    x = st.number_input("A number please:")
+    st.write("Just got", x)
 
 st.subheader("Input and display a number, show code")
 
 color = st.select_slider(
-    'Select a color of the rainbow',
-     options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-st.write('My favorite color is', color)
+    "Select a color of the rainbow",
+    options=["red", "orange", "yellow", "green", "blue", "indigo", "violet"],
+)
+st.write("My favorite color is", color)
 
 
 st.subheader("Slider")
 
-hour = st.slider('Hour', 0, 23, 12)
+hour = st.slider("Hour", 0, 23, 12)
 
 st.subheader("Display text, markdown, latex, variable, code")
 
-st.write('<hr>')
-st.text('Fixed width text')
-st.markdown('_Markdown_') # see *
-st.latex(r'e^{i\pi} + 1 = 0')
-st.write('Most objects') # df, err, func, keras!
-st.write(dict(a=1)) 
-st.write(['st', 'is <', 3]) # see *
-st.title('My title')
-st.header('My header')
-st.subheader('My sub')
-st.code('for i in range(8): foo()')
+st.write("<hr>")
+st.text("Fixed width text")
+st.markdown("_Markdown_")  # see *
+st.latex(r"e^{i\pi} + 1 = 0")
+st.write("Most objects")  # df, err, func, keras!
+st.write(dict(a=1))
+st.write(["st", "is <", 3])  # see *
+st.title("My title")
+st.header("My header")
+st.subheader("My sub")
+st.code("for i in range(8): foo()")
 
 st.subheader("Line break")
 
@@ -121,7 +131,8 @@ st.markdown("---")
 
 st.subheader("Graphviz chart")
 
-st.graphviz_chart('''
+st.graphviz_chart(
+    """
     digraph {
         run -> intr
         intr -> runbl
@@ -137,39 +148,37 @@ st.graphviz_chart('''
         new -> runmem
         sleep -> runmem
     }
-''')
+"""
+)
 
 st.subheader("Checkbox as collapse control")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
+if st.checkbox("Show raw data"):
+    st.subheader("Raw data")
 
 st.subheader("Input text")
 
 with st.echo():
-    name = st.text_input('Name')
+    name = st.text_input("Name")
     st.text(name)
 
 st.subheader("Show dataframe or table")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+st.write(
+    pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+)
 
 st.subheader("Now there is a chart")
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 st.line_chart(chart_data)
 
 st.subheader("Now there is a map")
 map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=["lat", "lon"]
+)
 st.map(map_data)
 
 
-st.header('Notes and discussion')
+st.header("Notes and discussion")
 
 """ 
 #### Limitations and feature requests
@@ -187,65 +196,74 @@ st.header('Notes and discussion')
 """
 
 st.title("Making TOC (experimental)")
-st.markdown("""
+st.markdown(
+    """
 Related links:
 
 - https://discuss.streamlit.io/t/table-of-contents-widget/3470/8?u=epogrebnyak
 - https://github.com/streamlit/streamlit/issues/726
 
-""")
+"""
+)
 
 
 class Header:
     tag: str = ""
-    
+
     def __init__(self, text: str):
         self.text = text
-    
+
     @property
     def id(self):
         """Create an identifcator from text."""
         return "".join(filter(str.isalnum, self.text)).lower()
-    
+
     @property
     def anchor(self):
         """Provide html text for anchored header. Example: 
            <h1 id="abcdef">Abc Def</h1>
         """
-        return f"<{self.tag} id='{self.id}'>{self.text}</{self.tag}>" 
+        return f"<{self.tag} id='{self.id}'>{self.text}</{self.tag}>"
 
     def toc_item(self) -> str:
         """Make markdown item for TOC listing. Example:
            '  - <a href='#abc'>Abc</a>'
         """
         return f"{self.spaces}- [{self.text}]('#{self.id}')"
-        
+
     @property
     def spaces(self):
-        return dict(h1="", h2=" " * 2, h3 = " " * 4).get(self.tag)
-    
-assert Header("abc").spaces is None    
+        return dict(h1="", h2=" " * 2, h3=" " * 4).get(self.tag)
+
+
+assert Header("abc").spaces is None
+
 
 class H1(Header):
     tag = "h1"
 
+
 class H2(Header):
     tag = "h2"
 
+
 assert H2("Abc").toc_item() == "  - [Abc]('#abc')"
+
 
 class H3(Header):
     tag = "h3"
+
 
 class TOC:
     """
     Original code, used with modifications:
     https://discuss.streamlit.io/t/table-of-contents-widget/3470/8?u=epogrebnyak
     """
+
     def __init__(self):
         self._headers = []
         self._placeholder = st.empty()
-    
+
     def title(self, text):
         self._add(H1(text))
 
@@ -255,10 +273,10 @@ class TOC:
     def subheader(self, text):
         self._add(H3(text))
 
-    def generate(self):    
+    def generate(self):
         text = "\n".join([h.toc_item() for h in self._headers])
         self._placeholder.markdown(text, unsafe_allow_html=True)
-    
+
     def _add(self, header):
         st.markdown(header.anchor, unsafe_allow_html=True)
         self._headers.append(header)
@@ -267,12 +285,13 @@ class TOC:
 class TOC_Sidebar(TOC):
     def __init__(self):
         self._headers = []
-        self._placeholder = st.sidebar.empty() 
-        
-  
+        self._placeholder = st.sidebar.empty()
+
+
 def blah():
     for a in range(3):
         st.write("Blabla...")
+
 
 toc = TOC()
 
